de475dcc414d474a5bb5cfa062c50f73072e189a
lwbvv <lwbvv@naver.com>
2020-01-29 15:06:51 +0000

    error push

--- testapp/assemble_model/scrap_model.py
+++
@@ -1,9 +0,0 @@
-from . import init_models
-from . import user_model
-from . import post_model
-
-class Scrap(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    created_date = models.DateTimeField(auto_now_add=True,)	
--- testapp/assemble_view/home_views.py
+++
@@ -1,5 +0,0 @@
-from testapp.assemble_view.__init__ import *
-
-
-
-# class Home(APIView):
